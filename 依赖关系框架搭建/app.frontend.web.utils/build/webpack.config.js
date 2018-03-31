e25544b2434d103480dcc353c47f18aa75f7f47e
liuyunxiang <827935541@qq.com>
2018-04-01 00:33:19 +0800

    优化了项目的依赖关系

--- 依赖关系框架搭建/app.frontend.web.utils/build/webpack.config.js
+++ 依赖关系框架搭建/app.frontend.web.utils/build/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
   },
-  target: 'node'
+  target: 'node',
+  mode: 'production'
 };
